aba6d67a779c914ffea2ea73a02d7ff8bd278a90
Abide Erülker <abide.erulker@galaksiya.com.tr>
2025-07-23 15:24:08 +0300

    mobile responsive scroll update

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -7,9 +7,7 @@ const isSidebarOpen = ref(false);
 </script>
 
 <template>
-  <div
-    class="flex flex-col lg:flex-row w-screen h-dvh max-w-screen overflow-hidden bg-[#F5F7FB]"
-  >
+  <div class="min-h-screen w-full max-w-screen overflow-y-auto bg-[#F5F7FB]">
     <div class="lg:hidden p-4">
       <button
         @click="isSidebarOpen = !isSidebarOpen"
@@ -18,24 +16,26 @@ const isSidebarOpen = ref(false);
         ☰
       </button>
     </div>
-    <div class="hidden lg:block px-4 py-4">
-      <Sidebar />
-    </div>
-    <div
-      v-if="isSidebarOpen"
-      class="fixed inset-0 z-50 bg-white px-4 py-4 shadow-xl lg:hidden"
-    >
-      <div class="flex justify-end mb-2">
-        <button @click="isSidebarOpen = false" class="text-xl">✕</button>
+    <div class="flex lg:flex-row">
+      <div class="hidden lg:block px-4 py-4">
+        <Sidebar />
       </div>
-      <Sidebar />
-    </div>
-    <div class="flex-1 flex flex-col">
-      <div class="pt-4 px-4 sm:px-6 md:px-8">
-        <NavHeader />
+      <div
+        v-if="isSidebarOpen"
+        class="fixed inset-0 z-50 bg-white px-4 py-4 shadow-xl lg:hidden overflow-y-auto"
+      >
+        <div class="flex justify-end mb-2">
+          <button @click="isSidebarOpen = false" class="text-xl">✕</button>
+        </div>
+        <Sidebar />
       </div>
-      <div class="flex-1 overflow-y-auto px-4 sm:px-6 md:px-8 pb-8">
-        <slot />
+      <div class="flex-1 flex flex-col">
+        <div class="pt-4 px-4 sm:px-6 md:px-8">
+          <NavHeader />
+        </div>
+        <div class="flex-1 px-4 sm:px-6 md:px-8 pb-8">
+          <slot />
+        </div>
       </div>
     </div>
   </div>
